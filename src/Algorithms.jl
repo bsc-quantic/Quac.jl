447926589b9d6a280becc4765fa9c31d50471997
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-09-02 13:04:50 +0200

    Init `Algorithms` module with `QFT`

---
+++ src/Algorithms.jl
@@ -0,0 +1,27 @@
+module Algorithms
+
+using Quac
+
+export QFT
+
+"""
+	QFT(n)
+
+Generate a Quantum Fourier Transform circuit of `n` qubits.
+"""
+QFT(n::Int) = begin
+	circ = Circuit(n)
+	for target in 1:n
+		push!(circ, H(target))
+
+		for control in target+1:n
+			m = control - target
+			θ = 2π / (2 ^ m)
+			push!(circ, CRz(control, target, θ))
+		end
+	end
+
+	circ
+end
+
+end
--- src/Quac.jl
+++ src/Quac.jl
@@ -3,5 +3,6 @@ module Quac
 include("Gate.jl")
 include("Array.jl")
 include("Circuit.jl")
+include("Algorithms.jl")
 
 end
