692b163493bc1a2883dafcb47b9a67dce04b6454
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-29 16:16:44 +0100

    Format code

--- src/Array.jl
+++ src/Array.jl
@@ -19,19 +19,24 @@ Matrix{F}(::Type{Td}) where {F} = Matrix{F}([1 0; 0 cispi(-1 // 4)])
 
 Matrix{T}(::Type{Swap}) = Matrix{T}([1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1])
 
-Matrix{T}(g::Rx) where {T} =
-    Matrix{T}([cos(g[:θ] / 2) -1im*sin(g[:θ] / 2); -1im*sin(g[:θ] / 2) cos(g[:θ] / 2)])
-Matrix{T}(g::Ry) where {T} =
-    Matrix{T}([cos(g[:θ] / 2) -sin(g[:θ] / 2); sin(g[:θ] / 2) cos(g[:θ] / 2)])
+Matrix{T}(g::Rx) where {T} = Matrix{T}([
+    cos(g[:θ] / 2) -1im*sin(g[:θ] / 2)
+    -1im*sin(g[:θ] / 2) cos(g[:θ] / 2)
+])
+Matrix{T}(g::Ry) where {T} = Matrix{T}([
+    cos(g[:θ] / 2) -sin(g[:θ] / 2)
+    sin(g[:θ] / 2) cos(g[:θ] / 2)
+])
 Matrix{T}(g::Rz) where {T} = Matrix{T}([1 0; 0 cis(g[:θ])])
 
-Matrix{T}(g::U2) where {T} = 1 / sqrt(2) * Matrix{T}([1 -cis(g[:λ]); cis(g[:ϕ]) cis(g[:ϕ] + g[:λ])])
-Matrix{T}(g::U3) where {T} = Matrix{T}(
-    [
-        cos(g[:θ] / 2) -cis(g[:λ])*sin(g[:θ] / 2)
-        cis(g[:ϕ])*sin(g[:θ] / 2) cis(g[:ϕ] + g[:λ])*cos(g[:θ] / 2)
-    ],
-)
+Matrix{T}(g::U2) where {T} = 1 / sqrt(2) * Matrix{T}([
+    1 -cis(g[:λ])
+    cis(g[:ϕ]) cis(g[:ϕ] + g[:λ])
+])
+Matrix{T}(g::U3) where {T} = Matrix{T}([
+    cos(g[:θ] / 2) -cis(g[:λ])*sin(g[:θ] / 2)
+    cis(g[:ϕ])*sin(g[:θ] / 2) cis(g[:ϕ] + g[:λ])*cos(g[:θ] / 2)
+])
 
 # TODO Matrix{T}(g::Control) = ...
 
