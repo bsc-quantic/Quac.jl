d95894b2dc951201a6a7a455f38c97a129c2d423
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-16 15:11:35 +0100

    Fix Aqua test with lazy load packages

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ include("Circuit_test.jl")
 
 import Quac
 using Aqua
-Aqua.test_all(Quac, ambiguities = false, piracy = false)
+Aqua.test_all(Quac, piracy = false, stale_deps = false)
 Aqua.test_ambiguities(Quac)
