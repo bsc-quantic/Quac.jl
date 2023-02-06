abd7a56e2f0ae2af0092abd2cb2be874f19eb80d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-02-06 14:59:51 +0100

    Refactor `QFT`

--- src/Algorithms.jl
+++ src/Algorithms.jl
@@ -9,15 +9,14 @@ export QFT
 
 Generate a Quantum Fourier Transform circuit of `n` qubits.
 """
-QFT(n::Int) = begin
+function QFT(n::Int)
     circuit = Circuit(n)
     for target in 1:n
         push!(circuit, H(target))
 
         for control in target+1:n
             m = control - target
-            θ = 2π / 2^m
-            push!(circuit, CRz(control, target, (θ = θ,)))
+            push!(circuit, CRz(control, target, (θ = 2π / 2^m,)))
         end
     end
 
