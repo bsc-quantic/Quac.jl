9a6140ce7bb09a2fe2f23c867989d2d596ed185a
Germán Navarro <ge.najim@gmail.com>
2023-04-11 17:07:44 +0200

    Add much more efficient version

--- src/Serializers/cQASM.jl
+++ src/Serializers/cQASM.jl
@@ -1,11 +1,144 @@
 module cQASM
 
 export parseCQASMCode
+# export buildTree
 
 # First imports
 import Automa
 import Automa.RegExp: @re_str
 const re = Automa.RegExp;
+
+# oneQubitGate = re"i|h|x|y|z|x90|y90|mx90|my90|s|sdag|t"
+# twoQubitGate = re"cnot|cz|swap"
+# threeQubitGate = re"toffoli"
+# rotationGate = re"rx|ry|rz|cr"
+# uniqueGate = re"crk"
+# oneQubitGate_bc = re"c-i|c-h|c-x|c-y|c-z|c-x90|c-y90|c-mx90|c-my90|c-s|c-sdag|c-t"
+# twoQubitGate_bc = re"c-cnot|c-cz|c-swap"
+# threeQubitGate_bc = re"c-toffoli"
+# rotationGate_bc = re"c-rx|c-ry|c-rz|c-cr"
+# uniqueGate_bc = re"c-crk"
+
+# keyword = re"version|qubits|map|prep|measure|display"
+# identifier = re"[A-Za-z_][0-9A-Za-z_]*"
+# comment = re"#[^\r\n]*"
+# newline = re"\r?\n"
+
+# minijulia = Automa.compile(
+#     re","               => :(emit(:comma)),
+#     re":"               => :(emit(:colon)),
+#     re"\."              => :(emit(:dot)),
+#     re"\("              => :(emit(:lparen)),
+#     re"\)"              => :(emit(:rparen)),
+#     re"\["              => :(emit(:lbracket)),
+#     re"]"               => :(emit(:rbracket)),
+#     re"{"               => :(emit(:lbrace)),
+#     re"}"               => :(emit(:rbrace)),
+#     re"\|"              => :(emit(:vbar)),
+#     re"[0-9]+\.[0-9]+"  => :(emit(:float)),
+#     re"[0-9]+"          => :(emit(:integer)),
+#     re"[\t ]+"          => :(emit(:spaces)),
+
+#     oneQubitGate        => :(emit(:oneQubitGate)),
+#     twoQubitGate        => :(emit(:twoQubitGate)),
+#     threeQubitGate      => :(emit(:threeQubitGate)),
+#     rotationGate        => :(emit(:rotationGate)),
+#     uniqueGate          => :(emit(:uniqueGate)),
+#     oneQubitGate_bc     => :(emit(:oneQubitGate_bc)),
+#     twoQubitGate_bc     => :(emit(:twoQubitGate_bc)),
+#     threeQubitGate_bc   => :(emit(:threeQubitGate_bc)),
+#     rotationGate_bc     => :(emit(:rotationGate_bc)),
+#     uniqueGate_bc       => :(emit(:uniqueGate_bc)),
+
+#     keyword             => :(emit(:keyword)),
+#     identifier          => :(emit(:identifier)),
+#     comment             => :(emit(:comment)),
+#     newline             => :(emit(:newline)),
+# )
+
+# context = Automa.CodeGenContext()
+
+# @eval function tokenize(data)
+#     $(Automa.generate_init_code(context, minijulia))
+#     p_end = p_eof = sizeof(data)
+#     tokens = Tuple{Symbol,String}[]
+#     emit(kind) = push!(tokens, (kind, data[ts:te]))
+#     while p ≤ p_eof && cs > 0
+#         $(Automa.generate_exec_code(context, minijulia))
+#     end
+#     if cs < 0
+#         error("failed to tokenize: " * data[ts:te])
+#     end
+#     return tokens
+# end
+
+# a = tokenize(readchomp("src/Serializers/test2.cq"))
+
+# struct TreeState
+#     inside_parens::Bool
+# end
+
+# function buildTree(data)
+#     tokens = tokenize(data)
+
+#     function look_for(::Type{T}, ownType) where T
+#         println(tokens[1])
+#     end
+
+#     function lookFor(::Type{Float64})
+
+#     end
+
+#     function lookFor(::Type{String})
+
+#     end
+
+#     while !isempty(tokens)
+#         token = popfirst!(tokens)
+#         println(token)
+
+#         symbol = token[1]
+#         string = token[2]
+
+#         if symbol == :comma
+    
+#         elseif symbol == :colon
+#         elseif symbol == :dot
+#         elseif symbol == :lparen
+#         elseif symbol == :rparen
+#         elseif symbol == :lbracket
+#         elseif symbol == :rbracket
+#         elseif symbol == :lbrace
+#             insideBraces = true
+#         elseif symbol == :rbrace
+#             insideBraces = false
+#         elseif symbol == :vbar
+#             if insideBraces
+#                 # return [reset all variables]
+#             end
+#         elseif symbol == :integer
+#         elseif symbol == :spaces
+#             popfirst!(tokens)
+#         elseif symbol == :keyword
+#             if string == "version "
+#                 lookfor(Float64)
+#             elseif string == "qubit "
+#             end
+#         elseif symbol == :identifier
+#         elseif symbol == :comment
+#         elseif symbol == :newline
+#             # return [reset all variables]
+#         end
+
+#     end
+# end
+
+
+
+
+
+
+
 
 machine = let
     # Define generic methods
@@ -16,11 +149,11 @@ machine = let
         return strRegEx
     end
 
-    function createStatement(statement::Symbol, statementName, params)
+    function createStatement(statementSymbol, statementName, params)
         @eval $statement = re.cat($statementName, $params)
     end
 
-    function createStatement(statement::Symbol, statementName, params, c_statementName, c_params)
+    function createStatement(statementSymbol, statementName, params, c_statementName, c_params)
         @eval $statement = re.cat($statementName, $params)
         @eval $(Symbol("c_" * String(statement))) = re.cat($c_statementName, $c_params)
     end
@@ -41,7 +174,7 @@ machine = let
     varName = re"[a-z_][a-z0-9_]*"
 
     tag = varName
-    idStreamSingle = re.cat(zeroOrMoreSpaces, oneOrMoreDigits, re.opt(re.cat(":", oneOrMoreDigits)), zeroOrMoreSpaces)
+    idStreamSingle = re.cat(zeroOrMoreSpaces, oneOrMoreDigits, re.opt(re.cat("", oneOrMoreDigits)), zeroOrMoreSpaces)
     idStreamMultiple = re.cat(idStreamSingle, re.rep(re.cat(",", idStreamSingle)))
     qubitIDStream = re.cat("q[", idStreamMultiple, "]")
     bitIDStream = re.cat("b[", idStreamMultiple, "]")
@@ -52,57 +185,84 @@ machine = let
 
     # 1.2 Gates
     # One qubit gates
-    for name in [:i, :h, :x, :y, :z, :x90, :y90, :mx90, :my90, :s, :sdag, :t]
-        gateNameRegEx = addActionToRegEx(re.parse(String(name)), :statementEnter, :statementExit)
-        paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, qid), :paramsEnter, :paramsExit)
-        
-        c_gateNameRegEx = addActionToRegEx(re.parse("c-" * String(name)), :statementEnter, :statementExit)
-        c_paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, controlBits, qid), :paramsEnter, :paramsExit)
+    one_qubit_gates_names = ["i", "h", "x", "y", "z", "x90", "y90", "mx90", "my90", "s", "sdag", "t"]
+    
+    names_non_controlled_re = re.parse(join(one_qubit_gates_names, "|"))
+    names_bit_controlled_re = re.parse(join(["c-" * gate for gate in one_qubit_gates_names], "|"))
+    params_non_controlled_re = re.cat(oneOrMoreSpaces, qid)
+    params_bit_controlled_re = re.cat(oneOrMoreSpaces, controlBits, qid)
 
-        createStatement(name, gateNameRegEx, paramsRegEx, c_gateNameRegEx, c_paramsRegEx)
-    end
+    one_qubit_gates_names_non_controlled = addActionToRegEx(names_non_controlled_re, :statementEnter, :statementExit)
+    one_qubit_gates_names_bit_controlled = addActionToRegEx(names_bit_controlled_re, :statementEnter, :statementExit)
+    one_qubit_gates_params_non_controlled = addActionToRegEx(params_non_controlled_re, :paramsEnter, :paramsExit)
+    one_qubit_gates_params_bit_controlled = addActionToRegEx(params_bit_controlled_re, :paramsEnter, :paramsExit)
+
+    one_qubit_gates_regExp_non_controlled = re.cat(one_qubit_gates_names_non_controlled, one_qubit_gates_params_non_controlled)
+    one_qubit_gates_regExp_bit_controlled = re.cat(one_qubit_gates_names_bit_controlled, one_qubit_gates_params_bit_controlled)
 
     # Two qubit gates
-    for name in [:cnot, :cz, :swap]
-        gateNameRegEx = addActionToRegEx(re.parse(String(name)), :statementEnter, :statementExit)
-        paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid), :paramsEnter, :paramsExit)
+    two_qubit_gates_names = ["cnot", "cz", "swap"]
 
-        c_gateNameRegEx = addActionToRegEx(re.parse("c-" * String(name)), :statementEnter, :statementExit)
-        c_paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid), :paramsEnter, :paramsExit)
+    names_non_controlled_re = re.parse(join(two_qubit_gates_names, "|"))
+    names_bit_controlled_re = re.parse(join(["c-" * gate for gate in two_qubit_gates_names], "|"))
+    params_non_controlled_re = re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid)
+    params_bit_controlled_re = re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid)
 
-        createStatement(name, gateNameRegEx, paramsRegEx, c_gateNameRegEx, c_paramsRegEx)
-    end
+    two_qubit_gates_names_non_controlled = addActionToRegEx(names_non_controlled_re, :statementEnter, :statementExit)
+    two_qubit_gates_names_bit_controlled = addActionToRegEx(names_bit_controlled_re, :statementEnter, :statementExit)
+    two_qubit_gates_params_non_controlled = addActionToRegEx(params_non_controlled_re, :paramsEnter, :paramsExit)
+    two_qubit_gates_params_bit_controlled = addActionToRegEx(params_bit_controlled_re, :paramsEnter, :paramsExit)
+
+    two_qubit_gates_regExp_non_controlled = re.cat(two_qubit_gates_names_non_controlled, two_qubit_gates_params_non_controlled)
+    two_qubit_gates_regExp_bit_controlled = re.cat(two_qubit_gates_names_bit_controlled, two_qubit_gates_params_bit_controlled)
 
     # Three qubit gates
-    for name in [:toffoli]
-        gateNameRegEx = addActionToRegEx(re.parse(String(name)), :statementEnter, :statementExit)
-        paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid), :paramsEnter, :paramsExit)
+    three_qubit_gates_names = ["toffoli"]
 
-        c_gateNameRegEx = addActionToRegEx(re.parse("c-" * String(name)), :statementEnter, :statementExit)
-        c_paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid), :paramsEnter, :paramsExit)
+    names_non_controlled_re = re.parse(join(three_qubit_gates_names, "|"))
+    names_bit_controlled_re = re.parse(join(["c-" * gate for gate in three_qubit_gates_names], "|"))
+    params_non_controlled_re = re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid)
+    params_bit_controlled_re = re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid)
 
-        createStatement(name, gateNameRegEx, paramsRegEx, c_gateNameRegEx, c_paramsRegEx)
-    end
+    three_qubit_gates_names_non_controlled = addActionToRegEx(names_non_controlled_re, :statementEnter, :statementExit)
+    three_qubit_gates_names_bit_controlled = addActionToRegEx(names_bit_controlled_re, :statementEnter, :statementExit)
+    three_qubit_gates_params_non_controlled = addActionToRegEx(params_non_controlled_re, :paramsEnter, :paramsExit)
+    three_qubit_gates_params_bit_controlled = addActionToRegEx(params_bit_controlled_re, :paramsEnter, :paramsExit)
+
+    three_qubit_gates_regExp_non_controlled = re.cat(three_qubit_gates_names_non_controlled, three_qubit_gates_params_non_controlled)
+    three_qubit_gates_regExp_bit_controlled = re.cat(three_qubit_gates_names_bit_controlled, three_qubit_gates_params_bit_controlled)
 
     # Rotation gates
-    for name in [:rx, :ry, :rz, :cr]
-        gateNameRegEx = addActionToRegEx(re.parse(String(name)), :statementEnter, :statementExit)
-        paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, angle), :paramsEnter, :paramsExit)
+    rotation_gates_names = ["rx", "ry", "rz", "cr"]
 
-        c_gateNameRegEx = addActionToRegEx(re.parse("c-" * String(name)), :statementEnter, :statementExit)
-        c_paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, angle), :paramsEnter, :paramsExit)
+    names_non_controlled_re = re.parse(join(rotation_gates_names, "|"))
+    names_bit_controlled_re = re.parse(join(["c-" * gate for gate in rotation_gates_names], "|"))
+    params_non_controlled_re = re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, angle)
+    params_bit_controlled_re = re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, angle)
 
-        createStatement(name, gateNameRegEx, paramsRegEx, c_gateNameRegEx, c_paramsRegEx)
-    end
+    rotation_gates_names_non_controlled = addActionToRegEx(names_non_controlled_re, :statementEnter, :statementExit)
+    rotation_gates_names_bit_controlled = addActionToRegEx(names_bit_controlled_re, :statementEnter, :statementExit)
+    rotation_gates_params_non_controlled = addActionToRegEx(params_non_controlled_re, :paramsEnter, :paramsExit)
+    rotation_gates_params_bit_controlled = addActionToRegEx(params_bit_controlled_re, :paramsEnter, :paramsExit)
 
-    # Another unique gates
-    gateNameRegEx = addActionToRegEx(re.parse("crk"), :statementEnter, :statementExit)
-    paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, natural), :paramsEnter, :paramsExit)
+    rotation_gates_regExp_non_controlled = re.cat(rotation_gates_names_non_controlled, rotation_gates_params_non_controlled)
+    rotation_gates_regExp_bit_controlled = re.cat(rotation_gates_names_bit_controlled, rotation_gates_params_bit_controlled)
 
-    c_gateNameRegEx = addActionToRegEx(re.parse("c-crk"), :statementEnter, :statementExit)
-    c_paramsRegEx = addActionToRegEx(re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, natural), :paramsEnter, :paramsExit)
+    # Another unique gates (crk)
+    unique_gates_names = ["crk"]
 
-    createStatement(:crk, gateNameRegEx, paramsRegEx, c_gateNameRegEx, c_paramsRegEx)
+    names_non_controlled_re = re.parse(join(unique_gates_names, "|"))
+    names_bit_controlled_re = re.parse(join(["c-" * gate for gate in unique_gates_names], "|"))
+    params_non_controlled_re = re.cat(oneOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, natural)
+    params_bit_controlled_re = re.cat(oneOrMoreSpaces, controlBits, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, qid, zeroOrMoreSpaces, ",", zeroOrMoreSpaces, natural)
+
+    unique_gates_names_non_controlled = addActionToRegEx(names_non_controlled_re, :statementEnter, :statementExit)
+    unique_gates_names_bit_controlled = addActionToRegEx(names_bit_controlled_re, :statementEnter, :statementExit)
+    unique_gates_params_non_controlled = addActionToRegEx(params_non_controlled_re, :paramsEnter, :paramsExit)
+    unique_gates_params_bit_controlled = addActionToRegEx(params_bit_controlled_re, :paramsEnter, :paramsExit)
+
+    unique_gates_regExp_non_controlled = re.cat(unique_gates_names_non_controlled, unique_gates_params_non_controlled)
+    unique_gates_regExp_bit_controlled = re.cat(unique_gates_names_bit_controlled, unique_gates_params_bit_controlled)
 
     # 1.3 Misc statements
     versionName = addActionToRegEx(re.parse("version"), :statementEnter, :statementExit)
@@ -113,7 +273,7 @@ machine = let
     numberQubitsParams = addActionToRegEx(re.cat(oneOrMoreSpaces, natural), :paramsEnter, :paramsExit)
     numberQubits = re.cat(numberQubitsName, numberQubitsParams)
     
-    comment = re.parse(raw"#[a-zA-Z0-9#!?$%&'()*+.,:;\-/\\_<>=@\[\]^`{}|~ ]*")    # ToDo: as it is now, comments does not include the chars ["¡¿´], and maybe some more...
+    comment = re.parse(raw"#[a-zA-Z0-9#!?$%&'()*+.,:;\-/\\_<>=@\[\]^`{}|~ ]*")    # ToDo as it is now, comments does not include the chars ["¡¿´], and maybe some more...
 
     mappingName = addActionToRegEx(re.parse("map"), :statementEnter, :statementExit)
     mappingParams = addActionToRegEx(re.cat(oneOrMoreSpaces, re.alt(bid, qid), zeroOrMoreSpaces, ",", zeroOrMoreSpaces, tag), :paramsEnter, :paramsExit)
@@ -140,18 +300,13 @@ machine = let
     subCircuitNumberIters = addActionToRegEx(re"[0-9]+", :numberItersEnter, :numberItersExit)
     subcircuit = addActionToRegEx(re.cat(".", varName, re.opt(re.cat("(", subCircuitNumberIters, ")"))), :statementEnter, :subcircuitExit)
 
-    gate = re.alt(i, h, x, y, z, x90, y90, mx90, my90, s, sdag, t,
-                    c_i, c_h, c_x, c_y, c_z, c_x90, c_y90, c_mx90, c_my90, c_s, c_sdag, c_t,
-                    cnot, cz, swap,
-                    c_cnot, c_cz, c_swap,
-                    toffoli,
-                    c_toffoli,
-                    rx, ry, rz, cr,
-                    c_rx, c_ry, c_rz, c_cr,
-                    crk,
-                    c_crk)
+    gate = re.alt(  one_qubit_gates_regExp_non_controlled, one_qubit_gates_regExp_bit_controlled,
+                    two_qubit_gates_regExp_non_controlled, two_qubit_gates_regExp_bit_controlled,
+                    three_qubit_gates_regExp_non_controlled, three_qubit_gates_regExp_bit_controlled,
+                    rotation_gates_regExp_non_controlled, rotation_gates_regExp_bit_controlled,
+                    unique_gates_regExp_non_controlled, unique_gates_regExp_bit_controlled)
 
-    line = re.cat(zeroOrMoreSpaces, re.alt(version, numberQubits, comment, mapping, prep, measure , display, subcircuit, gate), zeroOrMoreSpaces, re.opt(comment), newLine)
+    line = re.cat(zeroOrMoreSpaces, re.alt(version, numberQubits, comment, mapping, prep, measure, display, subcircuit, gate), zeroOrMoreSpaces, re.opt(comment), newLine)
     cqasmCode = re.rep(re.alt(line, re.cat(zeroOrMoreSpaces, newLine)))
 
     # Compile the regex to a FSM
@@ -211,16 +366,20 @@ context = Automa.CodeGenContext();
 
     $(Automa.generate_exec_code(context, machine, actions))
 
-    iszero(cs) || return string("failed to parse on byte ", p, "  [last chunk '", data[mark:p - 1], "' from string: '", data, "']")
+    iszero(cs) || return string("failed to parse on byte ", p, "  [last chunk '", data[mark:p - 1], "' from string '", data, "']")
     return statementsSet
 end;
 
-parseCQASMCode(String(readchomp("src/Serializers/test1.cq")))
+a = parseCQASMCode(String(readchomp("src/Serializers/test1.cq")))
 
 
-function paint_machine(regexp)
+function paint_machine(regexp::Automa.RegExp.RE)
     machine = Automa.compile(regexp)
-    write("C:/Users/German/Documents/Work/Quac.jl/src/Serializers/csv.dot", Automa.machine2dot(machine))
+    write("C:/Users/German/Documents/Work/Quac.jl/src/Serializers/csv1.dot", Automa.machine2dot(machine))
+end;
+
+function paint_machine(machine::Automa.Machine)
+    write("C:/Users/German/Documents/Work/Quac.jl/src/Serializers/csv1.dot", Automa.machine2dot(machine))
 end;
 
 end
