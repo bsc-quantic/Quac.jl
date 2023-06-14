c4e93dd94cc903a08c9062af0f0fbed79c506222
Jofre Vallès <jofrevalles99@gmail.com>
2023-06-14 09:40:01 +0200

    Add parsing function

--- src/Quac.jl
+++ src/Quac.jl
@@ -6,6 +6,7 @@ include("Gate.jl")
 include("Array.jl")
 include("Circuit.jl")
 include("Algorithms.jl")
+include("parser.jl")
 
 function __init__()
     @require Luxor = "ae8d54c2-7ccd-5906-9d76-62fc9837b5bc" include("Visualization.jl")
