83a77511162a485e287649dc2b07de8607f9596b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-07-14 12:30:43 +0200

    Create `Blueprint` recipe

--- ext/QuacMakieExt.jl
+++ ext/QuacMakieExt.jl
@@ -8,4 +8,26 @@ end
 
 using Makie
 
+@recipe(Blueprint, circuit) do scene
+    labels_theme = default_theme(scene, Makie.text)
+    Attributes(; default_theme(scene)..., lanecolor = :black, lanewidth = 2)
+end
+
+Makie.plottype(::Circuit) = Blueprint
+
+function Makie.plot!(bp::Blueprint)
+    circuit = bp[:circuit]
+
+    n = lanes(circuit[])
+    m = max(1, length(moments(circuit[])))
+
+    hlines!(bp, 1:n, xmax = m, color = bp[:lanecolor], linewidth = bp[:lanewidth])
+
+    for gate in circuit[]
+        poly!(bp, Rect())
+    end
+end
+
+function draw! end
+
 end
