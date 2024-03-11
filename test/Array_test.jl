817b2ef8ad86e3f31f38ec8a55683d7580d9d47a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-03-11 12:52:46 +0100

    Refactor array representation tests

--- test/Array_test.jl
+++ test/Array_test.jl
@@ -3,7 +3,7 @@
     import LinearAlgebra
 
     @testset "Matrix" begin
-        for Op in [
+        @testset "$Op()" for Op in [
             I,
             X,
             Y,
@@ -21,17 +21,12 @@
             Control{Control{Swap}},
             Control{Control{Control{Swap}}},
         ]
-            @test Matrix(Gate{Op}) isa Matrix{ComplexF32}
-            @test size(Matrix(Gate{Op})) == (2^length(Op), 2^length(Op))
+            N = length(Op)
+            @test Matrix(Op()) isa Matrix{ComplexF32}
+            @test size(Matrix(Op())) == (2^N, 2^N)
         end
 
-        # Special case for SU{N}
-        for N in [2, 4, 8]
-            @test Matrix(rand(Gate{SU{N}}, 1:Int(log2(N))...)) isa Matrix{ComplexF32}
-            @test size(Matrix(rand(Gate{SU{N}}, 1:Int(log2(N))...))) == (N, N)
-        end
-
-        for Op in [
+        @testset "rand($Op)" for Op in [
             I,
             X,
             Y,
@@ -59,20 +54,23 @@
             Control{Control{Control{Swap}}},
         ]
             N = length(Op)
-            gate = Gate{Op}(1:N...; rand(Op)...)
-            @test Matrix(gate) isa Matrix{ComplexF32}
-            @test size(Matrix(gate)) == (2^length(Op), 2^length(Op))
+            op = rand(Op)
+            @test Matrix(op) isa Matrix{ComplexF32}
+            @test size(Matrix(op)) == (2^length(Op), 2^length(Op))
+        end
+
+        # Special case for SU{N}
+        @testset "SU{$N}" for N in [1, 2, 3]
+            mat = Matrix(rand(SU{N}))
+            @test mat isa Matrix{ComplexF64}
+            @test size(mat) == (2^N, 2^N)
         end
     end
 
     @testset "Diagonal" begin
-        using LinearAlgebra: Diagonal, diag
+        using LinearAlgebra: Diagonal
 
-        for Op in [I, Z, S, Sd, T, Td, Control{Z}, Control{Control{Z}}, Control{Control{Control{Z}}}]
-            @test Diagonal(Gate{Op}) isa Diagonal{ComplexF32}
-        end
-
-        for Op in [
+        @testset "$Op" for Op in [
             I,
             Z,
             S,
@@ -88,14 +86,15 @@
             Control{Control{Rz}},
             Control{Control{Control{Rz}}},
         ]
-            gate = Gate{Op}(1:length(Op)...; rand(Op)...)
-            @test Diagonal(gate) isa Diagonal{ComplexF32}
-            @test size(Matrix(gate)) == (2^length(Op), 2^length(Op))
+            N = length(Op)
+            op = rand(Op)
+            @test Diagonal(op) isa Diagonal{ComplexF32}
+            @test size(Diagonal(op)) == (2^N, 2^N)
         end
     end
 
     @testset "Array" begin
-        for Op in [
+        @testset "$Op()" for Op in [
             I,
             X,
             Y,
@@ -113,19 +112,13 @@
             Control{Control{Swap}},
             Control{Control{Control{Swap}}},
         ]
-            N = length(Op) * 2
-            @test Array(Gate{Op}) isa Array{ComplexF32,N}
-            @test size(Array(Gate{Op})) == tuple(fill(2, N)...)
+            N = length(Op)
+            op = rand(Op)
+            @test Array(op) isa Array{ComplexF32,2N}
+            @test size(Array(op)) == tuple(fill(2, 2N)...)
         end
 
-        # Special case for SU{N}
-        for N in [2, 4, 8]
-            n_qubits = Int(log2(N))
-            @test Array(rand(Gate{SU{N}}, 1:Int(log2(N))...)) isa Array{ComplexF32,2 * n_qubits}
-            @test size(Array(rand(Gate{SU{N}}, 1:Int(log2(N))...))) == tuple(fill(2, 2 * n_qubits)...)
-        end
-
-        for Op in [
+        @testset "rand($Op)" for Op in [
             I,
             X,
             Y,
@@ -153,64 +146,74 @@
             Control{Control{Control{Swap}}},
         ]
             N = length(Op)
-            gate = Gate{Op}(1:N...; rand(Op)...)
+            op = rand(Op)
             @test Array(gate) isa Array{ComplexF32,2N}
             @test size(Array(gate)) == tuple(fill(2, 2N)...)
         end
+
+        # Special case for SU{N}
+        @testset "SU{$N}" for N in [1, 2, 3]
+            op = rand(SU{N})
+            @test Array(op) isa Array{ComplexF32,2N}
+            @test size(Array(op)) == tuple(fill(2, 2N)...)
+        end
     end
 
-    @testset "Correctness" begin
+    @testset "Parametric operators" begin
         @testset "Rxx" begin
             @test begin
-                g = Rxx(1, 2, θ = 0)
-                Matrix(g) ≈ LinearAlgebra.I(2^length(g))
+                op = Rxx(; θ = 0)
+                N = length(op)
+                Matrix(op) ≈ LinearAlgebra.I(2^N)
             end
 
             @test begin
-                g = Rxx(1, 2, θ = π / 2)
-                Matrix(g) ≈ 1 / sqrt(2) * [1 0 0 -1im; 0 1 -1im 0; 0 -1im 1 0; -1im 0 0 1]
+                op = Rxx(; θ = π / 2)
+                Matrix(op) ≈ 1 / sqrt(2) * [1 0 0 -1im; 0 1 -1im 0; 0 -1im 1 0; -1im 0 0 1]
             end
 
             @test begin
-                g = Rxx(1, 2, θ = π)
-                x = Matrix(Gate{X})
-                Matrix(g) ≈ -1im * reshape(permutedims(reshape(kron(vec(x), vec(x)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
+                op = Rxx(; θ = π)
+                x = Matrix(X())
+                Matrix(op) ≈ -1im * reshape(permutedims(reshape(kron(vec(x), vec(x)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
             end
         end
 
         @testset "Ryy" begin
             @test begin
-                g = Ryy(1, 2, θ = 0)
-                Matrix(g) ≈ LinearAlgebra.I(2^length(g))
+                op = Ryy(; θ = 0)
+                N = length(op)
+                Matrix(op) ≈ LinearAlgebra.I(2^N)
             end
 
             @test begin
-                g = Ryy(1, 2, θ = π / 2)
-                Matrix(g) ≈ 1 / sqrt(2) * [1 0 0 1im; 0 1 -1im 0; 0 -1im 1 0; 1im 0 0 1]
+                op = Ryy(; θ = π / 2)
+                Matrix(op) ≈ 1 / sqrt(2) * [1 0 0 1im; 0 1 -1im 0; 0 -1im 1 0; 1im 0 0 1]
             end
 
             @test begin
-                g = Ryy(1, 2, θ = π)
-                x = Matrix(Gate{Y})
-                Matrix(g) ≈ -1im * reshape(permutedims(reshape(kron(vec(x), vec(x)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
+                op = Ryy(; θ = π)
+                y = Matrix(Y())
+                Matrix(op) ≈ -1im * reshape(permutedims(reshape(kron(vec(y), vec(y)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
             end
         end
 
         @testset "Rzz" begin
             @test begin
-                g = Rzz(1, 2, θ = 0)
-                Matrix(g) ≈ LinearAlgebra.I(2^length(g))
+                op = Rzz(; θ = 0)
+                N = length(op)
+                Matrix(op) ≈ LinearAlgebra.I(2^N)
             end
 
             @test begin
-                g = Rzz(1, 2, θ = π / 2)
-                Matrix(g) ≈ 1 / sqrt(2) * Diagonal([1 - 1im, 1 + 1im, 1 + 1im, 1 - 1im])
+                op = Rzz(; θ = π / 2)
+                Matrix(op) ≈ 1 / sqrt(2) * Diagonal([1 - 1im, 1 + 1im, 1 + 1im, 1 - 1im])
             end
 
             @test begin
-                g = Rzz(1, 2, θ = π)
-                x = Matrix(Gate{Z})
-                Matrix(g) ≈ -1im * reshape(permutedims(reshape(kron(vec(x), vec(x)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
+                op = Rzz(; θ = π)
+                z = Matrix(Z())
+                Matrix(op) ≈ -1im * reshape(permutedims(reshape(kron(vec(z), vec(z)), 2, 2, 2, 2), (1, 3, 2, 4)), 4, 4)
             end
         end
     end
