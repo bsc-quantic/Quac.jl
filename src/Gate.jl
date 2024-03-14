cf787b1d3b6a90d250c210be5dd7575b892d71ad
Todorbsc <todor.krasimirov@bsc.es>
2024-03-14 15:19:44 +0100

    Set parameters in new targettype functions as type parameters

--- src/Gate.jl
+++ src/Gate.jl
@@ -378,8 +378,8 @@ targettype(::Type{Op}) where {Op<:Operator} = Op
 targettype(::Type{Control{Op}}) where {Op} = Op
 targettype(::Type{Control{Op}}) where {Op<:Control} = targettype(Op)
 targettype(::Type{<:Gate{Op}}) where {Op} = targettype(Op)
-targettype(g::Control{Op}) where {Op} = targettype(typeof(g))
-targettype(g::Gate{Op}) where {Op<:Control} = targettype(typeof(g))
+targettype(::Op) where {Op} = targettype(Op)
+targettype(::Gate{Op}) where {Op<:Operator} = targettype(Op)
 
 control(g::G) where {G<:Gate{<:Control}} = lanes(g)[1:end-length(targettype(G))]
 target(g::G) where {G<:Gate{<:Control}} = lanes(g)[end-length(targettype(G))+1:end]
